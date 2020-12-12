fa7128f358accf14108d116d40b3ff0e6729fe17
Will Coquillette <4646219+willquill@users.noreply.github.com>
2020-12-11 18:50:21 -0600

    Merged API request into a single function Added multiple API calls. Started mount list.

--- get.py
+++ get.py
@@ -1,48 +1,110 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # In Ubuntu 20.04, sudo apt install python3-dotenv
 import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-def create_access_token(client_id, client_secret, region):
-    url = "https://{}.battle.net/oauth/token".format(region)
+# Notes
+# In the API provided by Blizzard, there is no way to associate a mount with a specific faction, so that must be hardcoded.
+# I hardcoded this into faction_mounts.
+
+
+def get_json(region, client_id, client_secret, url):
+    # First get an access token
+    api_url = "https://{}.battle.net/oauth/token".format(region)
     body = {"grant_type": 'client_credentials'}
     auth = HTTPBasicAuth(client_id, client_secret)
+    token = requests.post(api_url, data=body, auth=auth).json().get(
+        'access_token', '')
 
-    response = requests.post(url, data=body, auth=auth)
-    return response.json()
+    # Now use token to query API
+    return requests.get(url, headers={
+        "Authorization": "Bearer {}".format(token)}).json()
 
-def get_profile_token(path, filename):
-    access_token = create_access_token(client_id, client_secret, u_region).get('access_token', '')
-    with open('/home/will/repos/wow-toons/{}'.format(filename), 'w') as json_file:
-        path = '/profile/{}'.format(path)
-        full_url = api_url+path+namespace_profile+locale
-        json.dump(requests.get(full_url, headers={"Authorization": "Bearer {}".format(access_token)}).json(), json_file)
 
 # Load variables from .env
 load_dotenv()
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
+app_id = os.getenv('CLIENT_ID')
+app_secret = os.getenv('CLIENT_SECRET')
 
 # Dynamic variables from user
 u_region = 'us'
-u_realm = 'dalaran'
-u_character = 'bezwulf'
+u_realm = 'archimonde'
+u_character = 'loopn'
 
 # Static variables
 token_url = 'https://{}.battle.net/oauth/token'.format(u_region)
-api_url = 'https://{}.api.blizzard.com'.format(u_region)
-redirect_uri = 'https://your.callback/uri'
+api_url_char = 'https://{}.api.blizzard.com/profile/wow/character/'.format(
+    u_region)
+api_url_data = 'https://{}.api.blizzard.com/data/wow/'.format(u_region)
 namespace_static = '?namespace=static-us'
 namespace_dynamic = '?namespace=dynamic-us'
 namespace_profile = '?namespace=profile-us'
 locale = '&locale=en_US'
 
-# Get achievement statistics
-get_profile_token('wow/character/{}/{}/achievements/statistics'.format(u_realm, u_character), 'stats.json')
-
 # Get appearance
-get_profile_token('wow/character/{}/{}/appearance'.format(u_realm, u_character), 'appearance.json')
+a = get_json(u_region, app_id, app_secret, api_url_char +
+             '{}/{}/{}{}{}'.format(u_realm, u_character, 'appearance', namespace_profile, locale))
 
 # Get mounts
-#get_profile_code('user/wow/collections/mounts', 'mounts.json')
+m = get_json(u_region, app_id, app_secret, api_url_char +
+             '{}/{}/{}{}{}'.format(u_realm, u_character, 'collections/mounts', namespace_profile, locale))
+
+# Get reputation
+r = get_json(u_region, app_id, app_secret, api_url_char +
+             '{}/{}/{}{}{}'.format(u_realm, u_character, 'reputations', namespace_profile, locale))
+
+faction_mounts = [{
+    "Faction": "Order of Embers",
+    "Side": "Alliance",
+    "Mounts": ["Dusky Waycrest Gryphon", "Smokey Charger"]
+}, {
+    "Faction": "Proudmoore Admiralty",
+    "Side": "Alliance",
+    "Mounts": ["Proudmoore Sea Crest", "Admiralty Stallion"]
+}, {
+    "Faction": "Storm's Wake",
+    "Side": "Alliance",
+    "Mounts": ["Stormsong Coastwatcher", "Dapple Gray"]
+}, {
+    "Faction": "Talanji's Expedition",
+    "Side": "Horde",
+    "Mounts": ["Expedition Bloodswarmer", "Captured Swampstalker"]
+}, {
+    "Faction": "Voldunai",
+    "Side": "Horde",
+    "Mounts": ["Alabaster Hyena", "Voldunai Dunescraper"]
+}, {
+    "Faction": "Zandalari Empire",
+    "Side": "Horde",
+    "Mounts": ["Cobalt Pterrordax", "Spectral Pterrorwing"]
+}
+    # add the 4 neutral factions
+]
+
+# Determine eligibility based on Exalted already
+for i in r['reputations']:
+    if i['faction']['name'] in bfa_factions_with_mounts:
+        if i['standing']['name'] == 'Exalted':
+            print('You are eligible for a mount from ' + i['faction']['name'])
+            # Add a function to see if you already have the mount
+
+# Get game mounts
+gm = get_json(u_region, app_id, app_secret, api_url_data +
+              '{}{}{}'.format('mount/index', namespace_static, locale))
+
+# Get game mounts specific
+gms = get_json(u_region, app_id, app_secret, api_url_data +
+               '{}{}{}'.format('mount/1060', namespace_static, locale))
+
+# Get game factions
+gf = get_json(u_region, app_id, app_secret, api_url_data +
+              '{}{}{}'.format('reputation-faction/index', namespace_static, locale))
+
+# Get game factions specific
+gfs = get_json(u_region, app_id, app_secret, api_url_data +
+               '{}{}{}'.format('reputation-faction/2158', namespace_static, locale))
+
+# For testing in case you want to save the json locally
+with open('/home/will/repos/wow-toons/{}'.format('whatever.json'), 'w') as json_file:
+    json.dump(gms, json_file)
