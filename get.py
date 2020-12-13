92912ed7765799377936823a713763dd2a647659
Will Coquillette <4646219+willquill@users.noreply.github.com>
2020-12-13 14:06:54 -0600

    add mounts, break mounts into separate dicts, still need special currency

--- get.py
+++ get.py
@@ -32,8 +32,8 @@ app_secret = os.getenv('CLIENT_SECRET')
 
 # Dynamic variables from user
 u_region = 'us'
-u_realm = 'dalaran'
-u_character = 'beznk'
+u_realm = 'hyjal'
+u_character = 'soulebreaker'
 
 # Static variables
 token_url = 'https://{}.battle.net/oauth/token'.format(u_region)
@@ -47,49 +47,7 @@ locale = '&locale=en_US'
 
 # Mount dict total
 # Note: Can refer to a list of keys only with [*mount_dict]
-mount_dict = {
-    "Dusky Waycrest Gryphon": {
-        "expansion": "Battle for Azeroth",
-        "faction": "Order of Embers",
-        "reputation": "Exalted",
-        "side": "Alliance",
-        "cost": 90000
-    },
-    "Smokey Charger": {
-        "expansion": "Battle for Azeroth",
-        "faction": "Order of Embers",
-        "reputation": "Exalted",
-        "side": "Alliance",
-        "cost": 10000
-    },
-    "Proudmoore Sea Scout": {
-        "expansion": "Battle for Azeroth",
-        "faction": "Proudmoore Admiralty",
-        "reputation": "Exalted",
-        "side": "Alliance",
-        "cost": 90000
-    },
-    "Admiralty Stallion": {
-        "expansion": "Battle for Azeroth",
-        "faction": "Proudmoore Admiralty",
-        "reputation": "Exalted",
-        "side": "Alliance",
-        "cost": 10000
-    },
-    "Stormsong Coastwatcher": {
-        "expansion": "Battle for Azeroth",
-        "faction": "Storm's Wake",
-        "reputation": "Exalted",
-        "side": "Alliance",
-        "cost": 90000
-    },
-    "Dapple Gray": {
-        "expansion": "Battle for Azeroth",
-        "faction": "Storm's Wake",
-        "reputation": "Exalted",
-        "side": "Alliance",
-        "cost": 10000
-    },
+mount_dict_horde = {
     "Expedition Bloodswarmer": {
         "expansion": "Battle for Azeroth",
         "faction": "Talanji's Expedition",
@@ -132,6 +90,102 @@ mount_dict = {
         "side": "Horde",
         "cost": 90000
     },
+    "Deathtusk Felboar": {
+        "expansion": "Warlords of Draenor",
+        "faction": "Vol'jin's Headhunters",
+        "reputation": "Exalted",
+        "side": "Horde",
+        "cost": 2000
+    },
+    "Swift Frostwolf": {
+        "expansion": "Warlords of Draenor",
+        "faction": "Frostwolf Orcs",
+        "reputation": "Exalted",
+        "side": "Horde",
+        "cost": 5000,
+        "cost_special": 5000,
+        "cost_special_currency": "Apexis Crystal" 
+    },
+    "Ironside Warwolf": {
+        "expansion": "Warlords of Draenor",
+        "faction": "Laughing Skull Orcs",
+        "reputation": "Exalted",
+        "side": "Horde",
+        "cost": 0
+    },
+}
+
+mount_dict_alliance = {
+    "Dusky Waycrest Gryphon": {
+        "expansion": "Battle for Azeroth",
+        "faction": "Order of Embers",
+        "reputation": "Exalted",
+        "side": "Alliance",
+        "cost": 90000
+    },
+    "Smokey Charger": {
+        "expansion": "Battle for Azeroth",
+        "faction": "Order of Embers",
+        "reputation": "Exalted",
+        "side": "Alliance",
+        "cost": 10000
+    },
+    "Proudmoore Sea Scout": {
+        "expansion": "Battle for Azeroth",
+        "faction": "Proudmoore Admiralty",
+        "reputation": "Exalted",
+        "side": "Alliance",
+        "cost": 90000
+    },
+    "Admiralty Stallion": {
+        "expansion": "Battle for Azeroth",
+        "faction": "Proudmoore Admiralty",
+        "reputation": "Exalted",
+        "side": "Alliance",
+        "cost": 10000
+    },
+    "Stormsong Coastwatcher": {
+        "expansion": "Battle for Azeroth",
+        "faction": "Storm's Wake",
+        "reputation": "Exalted",
+        "side": "Alliance",
+        "cost": 90000
+    },
+    "Dapple Gray": {
+        "expansion": "Battle for Azeroth",
+        "faction": "Storm's Wake",
+        "reputation": "Exalted",
+        "side": "Alliance",
+        "cost": 10000
+    },
+    "Dusty Rockhide": {
+        "expansion": "Warlords of Draenor",
+        "faction": "Council of Exarchs",
+        "reputation": "Exalted",
+        "side": "Alliance",
+        "cost": 5000,
+        "cost_special": 5000,
+        "cost_special_currency": "Apexis Crystal"
+    },
+    "Armored Irontusk": {
+        "expansion": "Warlords of Draenor",
+        "faction": "Sha'tari Defense",
+        "reputation": "Exalted",
+        "side": "Alliance",
+        "cost": 5000,
+        "cost_special": 5000,
+        "cost_special_currency": "Apexis Crystal" 
+    },
+    "Deathtusk Felboar": {
+        "expansion": "Warlords of Draenor",
+        "faction": "Hand of the Prophet",
+        "reputation": "Exalted",
+        "side": "Alliance",
+        "cost": 2000
+    },
+}
+
+mount_dict_neutral = {
     "Wastewander Skyterror": {
         "expansion": "Battle for Azeroth",
         "faction": "Ultum Accord",
@@ -151,17 +205,116 @@ mount_dict = {
         "faction": "The Unshackled",
         "reputation": "Exalted",
         "side": "Neutral",
-        "cost": 250,
-        "cost_currency": "Prismatic Manapearl"
+        "cost_special": 250,
+        "cost_special_currency": "Prismatic Manapearl"
     },
     "Ankoan Waveray": {
         "expansion": "Battle for Azeroth",
         "faction": "Waveblade Ankoan",
         "reputation": "Exalted",
         "side": "Neutral",
-        "cost": 250,
-        "cost_currency": "Prismatic Manapearl"
-    }
+        "cost_special": 250,
+        "cost_special_currency": "Prismatic Manapearl"
+    },
+    "Amethyst Ruinstrider": {
+        "expansion": "Legion",
+        "faction": "Argussian Reach",
+        "reputation": "Exalted",
+        "side": "Neutral",
+        "cost": 10000
+    },
+    "Beryl Ruinstrider": {
+        "expansion": "Legion",
+        "faction": "Argussian Reach",
+        "reputation": "Exalted",
+        "side": "Neutral",
+        "cost": 10000
+    },
+    "Cerulean Ruinstrider": {
+        "expansion": "Legion",
+        "faction": "Argussian Reach",
+        "reputation": "Exalted",
+        "side": "Neutral",
+        "cost": 10000
+    },
+    "Russet Ruinstrider": {
+        "expansion": "Legion",
+        "faction": "Argussian Reach",
+        "reputation": "Exalted",
+        "side": "Neutral",
+        "cost": 10000
+    },
+    "Sable Ruinstrider": {
+        "expansion": "Legion",
+        "faction": "Argussian Reach",
+        "reputation": "Exalted",
+        "side": "Neutral",
+        "cost": 10000
+    },
+    "Umber Ruinstrider": {
+        "expansion": "Legion",
+        "faction": "Argussian Reach",
+        "reputation": "Exalted",
+        "side": "Neutral",
+        "cost": 10000
+    },
+    "Brinedeep Bottom-Feeder": {
+        "expansion": "Legion",
+        "faction": "Conjurer Margoss",
+        "reputation": "Best Friend",
+        "side": "Neutral",
+        "cost_special": 100,
+        "cost_special_currency": "Drowned Mana"
+    },
+    "Lightforged Warframe": {
+        "expansion": "Legion",
+        "faction": "Army of the Light",
+        "reputation": "Exalted",
+        "side": "Neutral",
+        "cost": 500000
+    },
+    "Bristling Hellboar": {
+        "expansion": "Warlords of Draenor",
+        "faction": "The Saberstalkers",
+        "reputation": "Exalted",
+        "side": "Neutral",
+        "cost_special": 5000,
+        "cost_special_currency": "Blackfang Claw"
+    },
+    "Wild Goretusk": {
+        "expansion": "Warlords of Draenor",
+        "faction": "The Saberstalkers",
+        "reputation": "Honored",
+        "side": "Neutral",
+        "cost_special": 1000,
+        "cost_special_currency": "Blackfang Claw"
+    },
+    "Shadowmane Charger": {
+        "expansion": "Warlords of Draenor",
+        "faction": "Arakkoa Outcasts",
+        "reputation": "Exalted",
+        "side": "Neutral",
+        "cost": 5000,
+        "cost_special": 5000,
+        "cost_special_currency": "Apexis Crystal"
+    },
+    "Corrupted Dreadwing": {
+        "expansion": "Warlords of Draenor",
+        "faction": "Order of the Awakened",
+        "reputation": "Friendly",
+        "side": "Neutral",
+        "cost_special": 150000,
+        "cost_special_currency": "Apexis Crystal" 
+    },
+    "Domesticated Razorback": {
+        "expansion": "Warlords of Draenor",
+        "faction": "Steamwheedle Preservation Society",
+        "reputation": "Exalted",
+        "side": "Neutral",
+        "cost": 5000,
+        "cost_special": 5000,
+        "cost_special_currency": "Apexis Crystal"
+    },
 }
 
 #######################################
@@ -199,11 +352,14 @@ rep_list = get_json(u_region, app_id, app_secret, api_url_char +
 # Created flattened list of mounts
 mount_has = [i.get("mount", "").get("name", "") for i in mount_has] or []
 
-# Create list of missing mounts
-mount_missing = [i for i in [*mount_dict] if i not in mount_has]
+# Create combined dictionary
+mount_dict = {}
+if side == "Horde":
+    mount_dict = {**mount_dict_horde, **mount_dict_neutral}
+elif side == "Alliance":
+    mount_dict = {**mount_dict_alliance, **mount_dict_neutral}
 
-# Remove the opposings side's mounts from list
-mount_missing = [i for i in mount_missing if side in mount_dict.get(i).get("side") or "Neutral" in mount_dict.get(i).get("side")]
+mount_missing = [i for i in [*mount_dict] if i not in mount_has] or []
 
 # Sample item in rep_list:
 # {
@@ -225,34 +381,45 @@ mount_missing = [i for i in mount_missing if side in mount_dict.get(i).get("side
 
 # Dictionary showing the raw value needed to reach the tier
 rep_dict = {
+    "Neutral": 0,
     "Friendly": 6000,
     "Honored": 12000,
     "Revered": 21000,
-    "Exalted": 42000
+    "Exalted": 42000,
+    "Stranger": 0,
+    "Acquaintance": 8400,
+    "Buddy": 16800,
+    "Friend": 25200,
+    "Good Friend": 33600,
+    "Best Friend": 42000
 }
 
 # For each mount in missing list, create a dictionary of the mount, mount faction, and percentage toward tier
+
+# NEED TO MODIFY THIS TO ACCOUNT FOR SPECIAL CURRENCIES
+
 mount_missing_details = {}
 for mount in mount_missing:
-    for i in rep_list:
-        if mount_dict[mount].get("faction") == i.get("faction", "").get("name") or "":
+    for r in rep_list:
+        if mount_dict[mount].get("faction", "") or "" == r.get("faction", "").get("name", "") or "":
             mount_missing_details[mount] = {
                 "faction": mount_dict.get(mount, "").get("faction", "") or "",
-                "current_tier": i.get("standing", "").get("name"),
+                "current_tier": r.get("standing", "").get("name", "") or "",
                 "required_tier": mount_dict.get(mount, "").get("reputation", "") or "",
                 # standing/raw divided by required tier's value in rep_dict
-                "percent_complete": round(i.get("standing", "").get("raw")/rep_dict.get(mount_dict.get(mount, "").get("reputation", "") or ""), 2),
-                "required_gold": mount_dict.get(mount, "").get("cost", "") or ""
-        }
+                "percent_complete": round(r.get("standing", 0).get("raw", 0)/rep_dict.get(mount_dict.get(mount, "").get("reputation", "") or ""), 2) or 0,
+                "required_gold": mount_dict.get(mount, "").get("cost", "") or 0
+            }
 
 # Determine gold cost for eligible mount purchases
 total_gold = 0
 for k, v in mount_missing_details.items():
-    if v.get('percent_complete', '') or '' == 1.0:
+    if v.get('percent_complete', '') or '' >= 1.0:
         total_gold = total_gold + v.get('required_gold', '') or ''
 
 print("You have already met the reputation requirements to get some mounts you don't have.\n")
 print("To get all eligible mounts right now would cost you " + str('{:,}').format(total_gold) + " gold.")
+
 
 
 ############################################
